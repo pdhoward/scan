84788544ae329b98c91237dabc29e413fbdebfd2
pdhoward <patrick.howard@hotmail.com>
2020-10-12 15:04:32 -0400

    updates

--- README.md
+++ README.md
@@ -16,3 +16,34 @@ Signals can be streamed using stream project
 
 #### LICENSE
 MIT
+
+#### research
+
+* sample raw data packet
+02010610FF107803E8000000000000640023290009094536372045414145
+
+BLE data is decoded as follows:
+
+1st byte = length (n bytes)
+2nd byte = Types
+n-1 bytes = actual data
+And this repeats over the whole raw data. On above raw packet:
+
+1st Set:
+02: Length: 2 Bytes
+01: Type: Flags
+06: Flag - 02 && 04: LE General Discoverable && BR/EDR Not Supported
+
+2nd Set:
+10: Length: 16 bytes
+FF: Type: Manufacture Data
+107803E80000000000006400232900: Data specific to the manufacturer
+
+3rd Set:
+09: Length: 9 bytes
+09: Type: Complete Local Name
+4536372045414145: E67 EAAE (Name of device in ASCII)
+
+* more info here
+https://www.bluetooth.com/specifications/assigned-numbers/generic-access-profile/
+
--- xserver/index.js
+++ xserver/index.js
@@ -6,17 +6,28 @@ require('dotenv').config();
 ////// c strategic machines 2018 all rights reserved ///////
 ///////////////////////////////////////////////////////////
 
-const express =               require('express');
-const util =                  require('util')
-const { v4: uuidv4 } =        require('uuid');
-const {events} =              require('../events')
-const { g, b, gr, r, y } =    require('../console');
+const express =                require('express');
+const util =                   require('util')
+const advlib =                 require('advlib')
+const advlibble =              require('advlib-ble')
+const advlibbleservices =      require('advlib-ble-services')
+const advlibblemanufacturers = require('advlib-ble-manufacturers')
+
+const { v4: uuidv4 } =         require('uuid');
+const {events} =               require('../events')
+const { g, b, gr, r, y } =     require('../console');
 
 // Express app
 const app = express();
 const PORT = process.env.SCAN_PORT || 4200
 
 const log = (msg) => console.log(msg)
+
+const PROCESSORS = [
+  { processor: advlibble,
+    libraries: [ advlibbleservices, advlibblemanufacturers],                
+    options: { ignoreProtocolOverhead: true } }
+ ]
 //////////////////////////////////////////////////////////////////////////
 ////////////////////  Register Middleware       /////////////////////////
 ////////////////////////////////////////////////////////////////////////
@@ -70,6 +81,22 @@ const startScan = async() => {
 startScan()
 // let testmsg = 'Tests Started'
 // require('../test')(testmsg)
+
+
+//////////////////////////////////////////////////////////////////////////
+///////  Configuration and Processing of raw packets from BLE    ////////
+////////////////////////////////////////////////////////////////////////
+// let packets = [
+//   'c21d04acbe55daba16096164766c6962206279207265656c79416374697665',
+//   'c21904acbe55daba1216aafe10fc017265656c7961637469766507',
+//   'c21804acbe55daba1116aafe20000c4815200000004500000258',  
+// ];
+let packets = [    
+    '49B9CA61-3D7A-CDAE-0D49-1E3707D6391E'
+  ];
+
+let processedPackets = advlib.process(packets, PROCESSORS);
+console.log(processedPackets);
 
 app.listen(PORT, () => {
   console.log(g(`Server listening on port ${PORT}`)) 
